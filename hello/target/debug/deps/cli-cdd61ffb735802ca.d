d7eb7268af386b3b198ebb6ecf11a3034d0d76df
st4swift <st4swift@gmail.com>
2023-07-12 11:43:49 +0800

    lock

---
+++ hello/target/debug/deps/cli-cdd61ffb735802ca.d
@@ -0,0 +1,5 @@
+/home/st/rust/rust/hello/target/debug/deps/cli-cdd61ffb735802ca.rmeta: tests/cli.rs
+
+/home/st/rust/rust/hello/target/debug/deps/cli-cdd61ffb735802ca.d: tests/cli.rs
+
+tests/cli.rs:
---
+++ hello/target/debug/deps/hello-ba7f62d179adeace.d
@@ -0,0 +1,5 @@
+/home/st/rust/rust/hello/target/debug/deps/hello-ba7f62d179adeace.rmeta: src/main.rs
+
+/home/st/rust/rust/hello/target/debug/deps/hello-ba7f62d179adeace.d: src/main.rs
+
+src/main.rs:
