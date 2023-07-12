68c6927687b2656309a32b1b79d09bb6bf62c70b
st4swift <st4swift@gmail.com>
2023-07-12 11:45:52 +0800

    debug

---
+++ hello/target/debug/deps/hello-12cd205facd0b75b.d
@@ -0,0 +1,5 @@
+/home/st/rust/rust/hello/target/debug/deps/hello-12cd205facd0b75b: src/main.rs
+
+/home/st/rust/rust/hello/target/debug/deps/hello-12cd205facd0b75b.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/st/rust/rust/hello/target/debug/hello: /home/st/rust/rust/hello/src/main.rs
