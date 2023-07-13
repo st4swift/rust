f6b0b931093601bcc16fdbf6bf2b49a49273a87d
st4swift <st4swift@gmail.com>
2023-07-13 18:21:13 +0800

    713m1821

---
+++ hello/target/debug/deps/false-af517fd9457a39a7.d
@@ -0,0 +1,5 @@
+/Users/st/rust/rust/hello/target/debug/deps/false-af517fd9457a39a7: src/bin/false.rs
+
+/Users/st/rust/rust/hello/target/debug/deps/false-af517fd9457a39a7.d: src/bin/false.rs
+
+src/bin/false.rs:
