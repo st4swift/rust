af8973b921632d7a8fa6bfec15f371baefef8493
st4swift <st4swift@gmail.com>
2023-08-10 16:27:17 +0800

    100

--- rc1/src/main.rs
+++ rc1/src/main.rs
@@ -1,9 +1,24 @@
+
 fn main() {
-    let (a, mut b): (bool, bool) = (true, false);
-    println!("a = {:?}, b = {}", a, b);
-    b = true;
-    assert_eq!(a, b);
+    let x = '中'; 
+    println!("size of 中 is {}.", std::mem::size_of_val(&x));
+
+    let y = {
+        let x = 8;
+        x + 6
+    };
+    println!("y is {}", y);
+
+    assert_ne!((), ret_unit_type())
+
 }
+
+fn ret_unit_type(){
+    let x = 1;
+    let z = if x%2 == 1 {"odd"} else {"even"};
+    println!("x is {}, z is {}.", x, z);
+}
+
 
 
 
