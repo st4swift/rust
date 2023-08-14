9b3d5f96e8c520fb30a288ed9f4a35c899b9f821
st4swift <st4swift@gmail.com>
2023-08-14 11:32:08 +0800

    14

---
+++ rc2/target/debug/deps/rc2-3c14c020f4e0a2b0.d
@@ -0,0 +1,5 @@
+/home/st/rust/rust/rc2/target/debug/deps/rc2-3c14c020f4e0a2b0.rmeta: src/main.rs
+
+/home/st/rust/rust/rc2/target/debug/deps/rc2-3c14c020f4e0a2b0.d: src/main.rs
+
+src/main.rs:
---
+++ rc2/target/debug/deps/rc2-8e004dce509c2b22.d
@@ -0,0 +1,5 @@
+/home/st/rust/rust/rc2/target/debug/deps/rc2-8e004dce509c2b22.rmeta: src/main.rs
+
+/home/st/rust/rust/rc2/target/debug/deps/rc2-8e004dce509c2b22.d: src/main.rs
+
+src/main.rs:
